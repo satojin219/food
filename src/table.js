89504b2899684573cc8bbb98d413b0b922128839
satojin219 <seiron1212@outlook.jp>
2021-07-03 09:42:42 +0900

    テーブルの追加・削除機能を実装

---
+++ src/table.js
@@ -0,0 +1,34 @@
+// export class table {
+// addTableRow(){
+
+  const add=document.querySelector('.add');
+  add.addEventListener('click',()=>{
+  const table = document.querySelector('#table');
+  let row = table.insertRow(-1);
+  
+  let array = [];
+  let cellArray = [];
+  for (i = 0; i < 15; i++) {
+    array.push(i);
+    cellArray.push(i);
+    cellArray[i] = row.insertCell(array[i]);
+    
+  }
+  cellArray[0].innerHTML = '<input name="food" type="text" id="food" value="" placeholder="品名を入力"></input>';
+  cellArray[1].innerHTML = '<input id="number" type="text" class="weight" placeholder="重量を入力">';
+});
+// }
+
+// deleteTableRow(){
+
+  const minus=document.querySelector('.delete');
+  minus.addEventListener('click',()=>{
+    const table = document.querySelector('#table');
+    let row_num = table.rows.length;
+    if (row_num > 1) {
+      table.deleteRow(row_num - 1);
+    }
+  });
+// }
+
+// }
