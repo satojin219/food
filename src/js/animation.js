0a5360e3ad484955ad5812e17b0062835a14bb68
satojin219 <seiron1212@outlook.jp>
2021-08-12 08:15:40 +0900

    リファクタリングしました。

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,8 +1,67 @@
 
 
-function toggleAnimation(event, animateElement, animateClass) {
-  const i = document.querySelector(animateElement);
-  i.addEventListener(event, () => {
-    i.classList.toggle(animateClass);
-  });
-}
+// const calculateButton =document.querySelector('.button');
+// const edibleWeight    =document.querySelector('#weight');
+// const enteredFoodName =document.querySelector('#food').value;
+// const outPutWeight    =table.rows[1].cells;
+// let searchedFood= food_lists.filter((food_list) => {
+//   return (enteredFoodName == food_list.food_name);
+// });
+
+// const kcal    =searchedFood[0].enerc_kcal*(edibleWeight.value / 100);
+// const prot    =searchedFood[0].prot_*(edibleWeight.value / 100);
+// const fat     =searchedFood[0].fat_*(edibleWeight.value / 100);
+// const na      =searchedFood[0].na*(edibleWeight.value / 100);
+// const k       =searchedFood[0].k*(edibleWeight.value / 100);
+// const ca      =searchedFood[0].ca*(edibleWeight.value / 100);
+// const mg      =searchedFood[0].mg*(edibleWeight.value / 100);
+// const p       =searchedFood[0].fe*(edibleWeight.value / 100);
+// const ze      =searchedFood[0].ze*(edibleWeight.value / 100);
+// const cu      =searchedFood[0].cu*(edibleWeight.value / 100);
+// const vA      =searchedFood[0].retol*(edibleWeight.value / 100);
+// const vD      =searchedFood[0].vitd*(edibleWeight.value / 100);
+// const vK      =searchedFood[0].vitk*(edibleWeight.value / 100);
+// const vE      =searchedFood[0].tocpha*(edibleWeight.value / 100);
+// const vB1     =searchedFood[0].thia*(edibleWeight.value / 100);
+// const vB2     =searchedFood[0].ribf*(edibleWeight.value / 100);
+// const vB6     =searchedFood[0].vitb6*(edibleWeight.value / 100);
+// const vB12    =searchedFood[0].vitb12*(edibleWeight.value / 100);
+// const niasin  =searchedFood[0].ne*(edibleWeight.value / 100);
+// const pantoten=searchedFood[0].pantac*(edibleWeight.value / 100);
+// const yousan  =searchedFood[0].fol*(edibleWeight.value / 100);
+// const vC      =searchedFood[0].vitc*(edibleWeight.value / 100);
+// const fib     =searchedFood[0].fib_*(edibleWeight.value / 100);
+// const alc     =searchedFood[0].alc*(edibleWeight.value / 100);
+
+// function nutritionCalculate(nutrition,cellIndex,decimalPoint){
+//   return outPutWeight[cellIndex].innerHTML = nutrition.toFixed(decimalPoint);
+// }
+
+// calculateButton.addEventListener('click', () => {
+//   nutritionCalculate(kcal    ,0 ,0);
+//   nutritionCalculate(prot    ,1 ,1);
+//   nutritionCalculate(fat     ,2 ,1);
+//   nutritionCalculate(na      ,3 ,0);
+//   nutritionCalculate(k       ,4 ,0);
+//   nutritionCalculate(ca      ,5 ,0);
+//   nutritionCalculate(mg      ,6 ,0);
+//   nutritionCalculate(p       ,7 ,0);
+//   nutritionCalculate(ze      ,8 ,1);
+//   nutritionCalculate(cu      ,9 ,2);
+//   nutritionCalculate(vA      ,10,2);
+//   nutritionCalculate(vD      ,11,1);
+//   nutritionCalculate(vK      ,12,0);
+//   nutritionCalculate(vE      ,13,2);
+//   nutritionCalculate(vB1     ,14,2);
+//   nutritionCalculate(vB2     ,15,2);
+//   nutritionCalculate(vB6     ,16,1);
+//   nutritionCalculate(vB12    ,17,2);
+//   nutritionCalculate(niasin  ,18,1);
+//   nutritionCalculate(pantoten,19,2);
+//   nutritionCalculate(yousan  ,20,0);
+//   nutritionCalculate(vC      ,21,2);
+//   nutritionCalculate(fib     ,22,1);
+//   nutritionCalculate(alc     ,23,1);
+  
+
+// });
--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ const API = "https://apex.oracle.com/pls/apex/nutrition/food/food"
   const res        = await fetch(API);
   const json       = await res.json();
   const food_lists = await json.items;
-  // console.log(food_lists);
+  console.log(food_lists);
 
   let m =[];
   food_lists.forEach(food_list => {
@@ -21,68 +21,76 @@ $(function(){
 
 
 
+const calculateButton =document.querySelector('.button');
+const edibleWeight    =document.querySelector('#weight');
+const enteredFoodName =document.querySelector('#food');
+const outPutWeight    =table.rows[1].cells;
+function nutritionCalculate(nutrition,cellIndex,decimalPoint){
+  return outPutWeight[cellIndex].innerHTML = nutrition.toFixed(decimalPoint);
+}
 
-const calculateButton = document.querySelector('.button');
-  const num    = document.querySelector('#number');
-  const table  = document.querySelector('#table');
+calculateButton.addEventListener('click', () => {
+let searchedFood= food_lists.filter((food_list) => {
+  return (enteredFoodName.value == food_list.food_name);
+});
+console.log(searchedFood);
 
-  const rows   = table.rows;
-  const cells  = table.rows[1].cells;
+const kcal    =searchedFood[0].enerc_kcal*(edibleWeight.value / 100);
+const suger   =searchedFood[0].choavlm*(edibleWeight.value / 100);
+const prot    =searchedFood[0].prot_*(edibleWeight.value / 100);
+const fat     =searchedFood[0].fat_*(edibleWeight.value / 100);
+const na      =searchedFood[0].na*(edibleWeight.value / 100);
+const k       =searchedFood[0].k*(edibleWeight.value / 100);
+const ca      =searchedFood[0].ca*(edibleWeight.value / 100);
+const mg      =searchedFood[0].mg*(edibleWeight.value / 100);
+const p       =searchedFood[0].p*(edibleWeight.value / 100);
+const fe       =searchedFood[0].fe*(edibleWeight.value / 100);
+const zn      =searchedFood[0].zn*(edibleWeight.value / 100);
+const cu      =searchedFood[0].cu*(edibleWeight.value / 100);
+const vA      =searchedFood[0].retol*(edibleWeight.value / 100);
+const vD      =searchedFood[0].vitd*(edibleWeight.value / 100);
+const vK      =searchedFood[0].vitk*(edibleWeight.value / 100);
+const vE      =searchedFood[0].tocpha*(edibleWeight.value / 100);
+const vB1     =searchedFood[0].thia*(edibleWeight.value / 100);
+const vB2     =searchedFood[0].ribf*(edibleWeight.value / 100);
+const vB6     =searchedFood[0].vitb6a*(edibleWeight.value / 100);
+const vB12    =searchedFood[0].vitb12*(edibleWeight.value / 100);
+const niasin  =searchedFood[0].ne*(edibleWeight.value / 100);
+const pantoten=searchedFood[0].pantac*(edibleWeight.value / 100);
+const yousan  =searchedFood[0].fol*(edibleWeight.value / 100);
+const vC      =searchedFood[0].vitc*(edibleWeight.value / 100);
+const fib     =searchedFood[0].fib_*(edibleWeight.value / 100);
+const alc     =searchedFood[0].alc*(edibleWeight.value / 100);
 
-  calculateButton.addEventListener('click', () => {
-    const textbox = document.getElementById("food");
-    const value   = textbox.value;
+  nutritionCalculate(kcal    ,0 ,0);
+  nutritionCalculate(suger   ,1 ,1);
+  nutritionCalculate(prot    ,2 ,1);
+  nutritionCalculate(fat     ,3 ,1);
+  nutritionCalculate(na      ,4 ,0);
+  nutritionCalculate(k       ,5 ,0);
+  nutritionCalculate(ca      ,6 ,0);
+  nutritionCalculate(mg      ,7 ,0);
+  nutritionCalculate(p       ,8 ,0);
+  nutritionCalculate(fe      ,9 ,1);
+  nutritionCalculate(zn      ,10,1);
+  nutritionCalculate(cu      ,11,2);
+  nutritionCalculate(vA      ,12,2);
+  nutritionCalculate(vD      ,13,1);
+  nutritionCalculate(vK      ,14,0);
+  nutritionCalculate(vE      ,15,2);
+  nutritionCalculate(vB1     ,16,2);
+  nutritionCalculate(vB2     ,17,2);
+  nutritionCalculate(vB6     ,18,1);
+  nutritionCalculate(vB12    ,19,2);
+  nutritionCalculate(niasin  ,20,1);
+  nutritionCalculate(pantoten,21,2);
+  nutritionCalculate(yousan  ,22,0);
+  nutritionCalculate(vC      ,23,2);
+  nutritionCalculate(fib     ,24,1);
+  nutritionCalculate(alc     ,25,1);
+  
 
-    let food_list = food_lists.filter((item) => {
-      return (value == item.food_name);
-    });
+});
 
-
-    const kcal = food_list[0].enerc_kcal * (num.value / 100);
-    const prot = food_list[0].prot_ * (num.value / 100);
-    const fat  = food_list[0].fat_ * (num.value / 100);
-    const na    = food_list[0].na * (num.value / 100);
-    const k    = food_list[0].k * (num.value / 100);
-    const ca   = food_list[0].ca * (num.value / 100);
-    const mg   = food_list[0].mg * (num.value / 100);
-    const p   = food_list[0].p * (num.value / 100);
-    const fe   = food_list[0].fe * (num.value / 100);
-    const ze   = food_list[0].zn * (num.value / 100);
-    const cu   = food_list[0].cu * (num.value / 100);
-    const vA   = food_list[0].retol * (num.value / 100);
-    const vD   = food_list[0].vitd * (num.value / 100);
-    const vK   = food_list[0].vitk * (num.value / 100);
-    const  vE = food_list[0].tocpha * (num.value / 100);
-    const vB1  = food_list[0].thia * (num.value / 100);
-    const vB2  = food_list[0].ribf * (num.value / 100);
-    const vB6  = food_list[0].vitb6 * (num.value / 100);
-    const vB12  = food_list[0].vitb12 * (num.value / 100);
-    const niasin  = food_list[0].ne * (num.value / 100);
-    const pantoten  = food_list[0].pantac * (num.value / 100);
-    const yousan  = food_list[0].fol * (num.value / 100);
-    const vC   = food_list[0].vitc * (num.value / 100);
-    const fib  = food_list[0].fib_ * (num.value / 100);
-    const alc  = food_list[0].alc * (num.value / 100);
-
-    const round = (index, nutrition, decimal) => {
-      return cells[index].innerHTML = nutrition.toFixed(decimal);
-    }
-    round(0, kcal, 0);
-    round(1, prot, 1);
-    round(2, fat, 1);
-    round(3, ca, 0);
-    round(4, fe, 1);
-    round(5, k, 0);
-    round(6, ze, 1);
-    round(7, vA, 2);
-    round(7, vD, 2);
-    round(7, vK, 2);
-    round(7, vE, 2);
-    round(8, vB1, 2);
-    round(9, vB2, 2);
-    round(10, vC, 2);
-    round(11, fib, 1);
-    round(12, alc, 1);
-  });
 }
 fetchApi();
