f9bb3f4c2f2793c1243b740f0880b2df7f84757b
satojin219 <seiron1212@outlook.jp>
2021-08-12 19:30:30 +0900

    webpackの実装

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import { calculate } from "./modules/calculate.js";
 import {clear} from"./modules/reload.js";
+import '../css/style.scss';
+
 clear();
 calculate();
--- src/js/modules/fetch.js
+++ src/js/modules/fetch.js
@@ -3,6 +3,7 @@
 //  async function fetchApi() {
   const API = "https://apex.oracle.com/pls/apex/nutrition/food/food"
   const res        = await fetch(API);
+  // , { mode: 'cors',credentials: 'include' }
   const json       = await res.json();
   export const food_lists = await json.items;
   console.log(food_lists);
